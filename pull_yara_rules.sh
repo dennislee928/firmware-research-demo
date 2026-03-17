6bf900e73e12ede5d02edcc8df8894cc87f6c4cf
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-03-17 11:41:33 +0800

    Update pull_yara_rules.sh

--- pull_yara_rules.sh
+++ pull_yara_rules.sh
@@ -9,24 +9,26 @@ mkdir -p "$TEMP_DIR"
 echo "Updating YARA rules..."
 
 # List of official/authoritative rule URLs
+# Keys cannot contain dots in some bash versions/shells, using underscores
 declare -A RULES=(
-    ["APT_Equation.yar"]="https://raw.githubusercontent.com/Yara-Rules/rules/master/malware/APT_Equation.yar"
-    ["spy_equation_fiveeyes.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/spy_equation_fiveeyes.yar"
-    ["apt_vpnfilter.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_vpnfilter.yar"
-    ["apt_blackenergy.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_blackenergy.yar"
-    ["apt_fancybear_dnc.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_fancybear_dnc.yar"
-    ["apt_waterbear.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_waterbear.yar"
-    ["apt_plead_downloader.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_plead_downloader.yar"
-    ["gen_tscookie_rat.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/gen_tscookie_rat.yar"
-    ["apt_venom_linux_rootkit.yar"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_venom_linux_rootkit.yar"
-    ["Windows_Trojan_Generic.yar"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Windows_Trojan_Generic.yar"
-    ["Linux_Trojan_Generic.yar"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Linux_Trojan_Generic.yar"
-    ["Windows_Trojan_Trickbot.yar"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Windows_Trojan_Trickbot.yar"
-    ["Linux_Trojan_Mirai.yar"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Linux_Trojan_Mirai.yar"
+    ["APT_Equation"]="https://raw.githubusercontent.com/Yara-Rules/rules/master/malware/APT_Equation.yar"
+    ["spy_equation_fiveeyes"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/spy_equation_fiveeyes.yar"
+    ["apt_vpnfilter"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_vpnfilter.yar"
+    ["apt_blackenergy"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_blackenergy.yar"
+    ["apt_fancybear_dnc"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_fancybear_dnc.yar"
+    ["apt_waterbear"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_waterbear.yar"
+    ["apt_plead_downloader"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_plead_downloader.yar"
+    ["gen_tscookie_rat"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/gen_tscookie_rat.yar"
+    ["apt_venom_linux_rootkit"]="https://raw.githubusercontent.com/Neo23x0/signature-base/master/yara/apt_venom_linux_rootkit.yar"
+    ["Windows_Trojan_Generic"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Windows_Trojan_Generic.yar"
+    ["Linux_Trojan_Generic"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Linux_Trojan_Generic.yar"
+    ["Windows_Trojan_Trickbot"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Windows_Trojan_Trickbot.yar"
+    ["Linux_Trojan_Mirai"]="https://raw.githubusercontent.com/elastic/protections-artifacts/main/yara/rules/Linux_Trojan_Mirai.yar"
 )
 
-for filename in "${!RULES[@]}"; do
-    url="${RULES[$filename]}"
+for key in "${!RULES[@]}"; do
+    url="${RULES[$key]}"
+    filename="${key}.yar"
     echo "Downloading $filename..."
     curl -sL "$url" -o "$TEMP_DIR/$filename"
     
